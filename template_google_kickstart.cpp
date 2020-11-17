a8d5e17f377b5e10f2dae36fa6deeee86e2ec4de
Navin Shrinivas <karupal2002@gmail.com>
2020-11-17 10:52:41 +0000

    adding templates and planning for modules

---
+++ template_google_kickstart.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    int t;
+    int tc=0;
+    cin>>t;
+    IOS;
+    while(t)
+    {
+        tc++;
+        cout<<"Case #"<<tc<<": "<<count<<"\n";
+        t--;
+    }
+}
